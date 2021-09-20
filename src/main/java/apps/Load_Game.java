73876e88fa5863f3b2bc738c565234b4f0e59556
Tim van der Graaff <timvdg45@gmail.com>
2021-09-19 21:27:20 -0500

    Update Load_Game.java

--- src/main/java/apps/Load_Game.java
+++ src/main/java/apps/Load_Game.java
@@ -1,1 +1,61 @@
+//Author: Timothy van der Graaff
+package apps;
 
+import configuration.Config;
+import controllers.Control_Load_Game;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/load-game")
+public class Load_Game {
+    
+    @RequestMapping(method = RequestMethod.POST)
+    String home(
+            @RequestParam(value = "game_id", defaultValue = "") String game_id,
+            @RequestParam(value = "player_session", defaultValue = "") String player_session,
+            @RequestParam(value = "load_game", defaultValue = "") String load_game
+    ) {
+        
+        Connection use_open_connection;
+        
+        try {
+            
+            use_open_connection = Config.openConnection();
+            
+            Control_Load_Game.use_connection = use_open_connection;
+            Control_Load_Game.game_id = game_id;
+            Control_Load_Game.player_session = player_session;
+            
+            if (load_game.equals("Load game")) {
+                
+                return Control_Load_Game.control_search_occupied_game_spaces();
+            } else {
+                
+                return "";
+            }
+        } catch (IOException e) {
+            
+            return "";
+        }
+    }
+	
+    public static void main(String[] args) throws Exception, IOException {
+		
+        SpringApplication.run(Load_Game.class, args);
+    }
+}
