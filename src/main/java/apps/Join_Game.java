f5ada4852e60382482532f2fbec08fa8d8e0664a
Tim van der Graaff <timvdg45@gmail.com>
2021-09-29 03:20:37 -0500

    Create Join_Game.java

---
+++ src/main/java/apps/Join_Game.java
@@ -0,0 +1,71 @@
+//Author: Timothy van der Graaff
+package apps;
+
+import configuration.Config;
+import controllers.Control_Save_Game;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/join-game")
+public class Join_Game {
+    
+    @RequestMapping(method = RequestMethod.POST)
+    String home(
+            @RequestParam(value = "player_full_name", defaultValue = "") String player_full_name,
+            @RequestParam(value = "player_session", defaultValue = "") String player_session,
+            @RequestParam(value = "game_id", defaultValue = "") String game_id,
+            @RequestParam(value = "add_game", defaultValue = "") String add_game
+    ) {
+        
+        Connection use_open_connection;
+        
+        DateTimeFormatter time_format = DateTimeFormatter.ofPattern("hh:mm a 'EST'");
+          
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now(ZoneId.of("America/New_York"));
+        
+        try {
+            
+            use_open_connection = Config.openConnection();
+            
+            Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
+            Control_Save_Game.player_full_name = player_full_name;
+            Control_Save_Game.player_session = player_session;
+            Control_Save_Game.game_id = game_id;
+            Control_Save_Game.date_received = String.valueOf(localDate);
+            Control_Save_Game.time_received = String.valueOf(time_format.format(localTime));
+            Control_Save_Game.join_game = join_game;
+            
+            if (join_game.equals("Join game")) {
+                
+                return Control_Save_Game.control_join_game();
+            } else {
+                
+                return "";
+            }
+        } catch (IOException e) {
+            
+            return "";
+        }
+    }
+	
+    public static void main(String[] args) throws Exception, IOException {
+		
+        SpringApplication.run(Join_Game.class, args);
+    }
+}
