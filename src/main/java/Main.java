81d8e6f7ef32eaa130280e86f69bc62fd643401d
Tim van der Graaff <timvdg45@gmail.com>
2021-10-02 17:37:30 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Object[] sources = new Object[2];
+        Object[] sources = new Object[6];
         
         sources[0] = Directory_Hider.class;
-        sources[1] = apps.Load_Game.class;
+		sources[1] = apps.Add_Game.class;
+		sources[2] = apps.Add_Game_Piece.class;
+		sources[3] = apps.Join_Game.class;
+        sources[4] = apps.Load_Game.class;
+		sources[5] = apps.Search_Available_Games.class;
 		
         SpringApplication.run(sources, args);
     }
