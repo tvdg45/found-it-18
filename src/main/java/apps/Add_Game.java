49e9f8132fb04317ffcc5decdce5004f39c7c09c
Tim van der Graaff <timvdg45@gmail.com>
2021-11-02 03:57:23 -0500

    Update Add_Game.java

--- src/main/java/apps/Add_Game.java
+++ src/main/java/apps/Add_Game.java
@@ -28,6 +28,7 @@ public class Add_Game {
     String home(
             @RequestParam(value = "player_full_name", defaultValue = "") String player_full_name,
             @RequestParam(value = "player_session", defaultValue = "") String player_session,
+			@RequestParam(value = "player_chosen_game_piece", defaultValue = "") String player_chosen_game_piece,
             @RequestParam(value = "add_game", defaultValue = "") String add_game
     ) {
         
@@ -45,6 +46,7 @@ public class Add_Game {
             Control_Save_Game.use_connection = use_open_connection;
             Control_Save_Game.player_full_name = player_full_name;
             Control_Save_Game.player_session = player_session;
+			Control_Save_Game.player_chosen_game_piece = player_chosen_game_piece;
             Control_Save_Game.date_received = String.valueOf(localDate);
             Control_Save_Game.time_received = String.valueOf(time_format.format(localTime));
             Control_Save_Game.add_game = add_game;
