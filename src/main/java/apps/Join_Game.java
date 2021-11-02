957022480e738d45f72d2312c231981a8483ad68
Tim van der Graaff <timvdg45@gmail.com>
2021-11-02 04:01:41 -0500

    Update Join_Game.java

--- src/main/java/apps/Join_Game.java
+++ src/main/java/apps/Join_Game.java
@@ -28,6 +28,7 @@ public class Join_Game {
     String home(
             @RequestParam(value = "player_full_name", defaultValue = "") String player_full_name,
             @RequestParam(value = "player_session", defaultValue = "") String player_session,
+			@RequestParam(value = "player_chosen_game_piece", defaultValue = "") String player_chosen_game_piece,
             @RequestParam(value = "game_id", defaultValue = "") String game_id,
             @RequestParam(value = "join_game", defaultValue = "") String join_game
     ) {
@@ -46,6 +47,7 @@ public class Join_Game {
             Control_Save_Game.use_connection = use_open_connection;
             Control_Save_Game.player_full_name = player_full_name;
             Control_Save_Game.player_session = player_session;
+			Control_Save_Game.player_chosen_game_piece = player_chosen_game_piece;
             Control_Save_Game.game_id = game_id;
             Control_Save_Game.date_received = String.valueOf(localDate);
             Control_Save_Game.time_received = String.valueOf(time_format.format(localTime));
