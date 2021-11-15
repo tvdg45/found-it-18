78ddb8eec8146daedd6114beb84e93538191f6a3
Tim van der Graaff <timvdg45@gmail.com>
2021-11-15 03:51:18 -0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Object[] sources = new Object[6];
+        Object[] sources = new Object[7];
         
         sources[0] = Directory_Hider.class;
 		sources[1] = apps.Add_Game.class;
