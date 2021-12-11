ec0f2b9bdb1848a33169befe763613e57ea70f86
Tim van der Graaff <timvdg45@gmail.com>
2021-12-11 07:41:19 -0600

    Add files via upload

--- src/main/java/views/Show_Loaded_Game.java
+++ src/main/java/views/Show_Loaded_Game.java
@@ -11,6 +11,9 @@ public class Show_Loaded_Game {
     public static ArrayList<String> available_games = new ArrayList<>();
     public static ArrayList<String> all_players = new ArrayList<>();
     public static ArrayList<ArrayList<String>> other_player = new ArrayList<>();
+    public static ArrayList<ArrayList<String>> game_player_information = new ArrayList<>();
+    public static ArrayList<ArrayList<String>> game_players = new ArrayList<>();
+    public static ArrayList<ArrayList<String>> game_players_whose_turn = new ArrayList<>();
     public static ArrayList<ArrayList<String>> occupied_game_spaces = new ArrayList<>();
     
     public static String show_available_games() {
@@ -128,6 +131,11 @@ public class Show_Loaded_Game {
         ArrayList<String> find = new ArrayList<>();
         ArrayList<String> replace = new ArrayList<>();
         
+        int game_player_information_count = 0;
+        int players_per_game = 0;
+        int game_player_turns = 0;
+        int game_spaces_taken = 0;
+        
         find.add("<script");
         find.add("<style");
         find.add("\"");
@@ -146,7 +154,70 @@ public class Show_Loaded_Game {
         replace.add("");
         replace.add("");
         
-        output += "[";
+        output += "{\"game_player_information\": [";
+        
+        for (int i = 0; i < game_player_information.get(0).size(); i++) {
+            
+            output += "{\"player_id\": \"" +
+                    Find_and_replace.find_and_replace(find, replace, String.valueOf(game_player_information.get(0).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
+                    "\", \"player_full_name\": \"" +
+                    Find_and_replace.find_and_replace(find, replace, String.valueOf(game_player_information.get(1).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
+                    "\", \"player_chosen_game_piece\": \"" +
+                    Find_and_replace.find_and_replace(find, replace, String.valueOf(game_player_information.get(2).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
+                    "\"}, ";
+            
+            game_player_information_count++;
+        }
+        
+        if (game_player_information_count != 1) {
+            
+            output += "{\"player_id\": \"no players\", \"player_full_name\": \"no players\", " +
+                    "\"player_chosen_game_piece\": \"no players\"}, ";
+        }
+        
+        output += "{}], ";
+        output += "\"game_players\": [";
+        
+        for (int i = 0; i < game_players.get(0).size(); i++) {
+            
+            output += "{\"player_id\": \"" +
+                    Find_and_replace.find_and_replace(find, replace, String.valueOf(game_players.get(0).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
+                    "\", \"player_full_name\": \"" +
+                    Find_and_replace.find_and_replace(find, replace, String.valueOf(game_players.get(1).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
+                    "\", \"player_chosen_game_piece\": \"" +
+                    Find_and_replace.find_and_replace(find, replace, String.valueOf(game_players.get(2).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
+                    "\"}, ";
+            
+            players_per_game++;
+        }
+        
+        if (players_per_game != 2) {
+            
+            output += "{\"player_id\": \"no players\", \"player_full_name\": \"no players\", " +
+                    "\"player_chosen_game_piece\": \"no players\"}, ";
+        }
+        
+        output += "{}], ";
+        output += "\"game_player_turns\": [";
+        
+        for (int i = 0; i < game_players_whose_turn.get(0).size(); i++) {
+            
+            output += "{\"player_id\": \"" +
+                    Find_and_replace.find_and_replace(find, replace, String.valueOf(game_players_whose_turn.get(0).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
+                    "\", \"player_has_turn\": \"" +
+                    Find_and_replace.find_and_replace(find, replace, String.valueOf(game_players_whose_turn.get(1).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
+                    "\"}, ";
+            
+            game_player_turns++;
+        }
+        
+        if (game_player_turns != 2) {
+            
+            output += "{\"player_id\": \"no players\", \"player_has_turn\": \"no players\"}, ";
+        }
+       
+        output += "{}], ";
+        output += "\"game_board\": [";
         
         for (int i = 0; i < occupied_game_spaces.get(0).size(); i++) {
             
@@ -159,9 +230,19 @@ public class Show_Loaded_Game {
                     "\", \"player_session\": \"" +
                     Find_and_replace.find_and_replace(find, replace, String.valueOf(occupied_game_spaces.get(3).get(i)).replace("<", "&lt;").replace(">", "&gt;")) +
                     "\"}, ";
+            
+            game_spaces_taken++;
         }
         
-        output += "{}]";
+        if (game_spaces_taken == 0) {
+            
+            output += "{\"game_id\": \"no game spaces occupied\", " +
+                    "\"player_chosen_game_piece\": \"no game spaces occupied\", " +
+                    "\"player_chosen_game_space\": \"no game spaces occupied\", " +
+                    "\"player_session\": \"no game spaces occupied\"}, ";
+        }
+        
+        output += "{}]}";
         
         output = output.replace(", {}", ""); 
         
@@ -172,9 +253,19 @@ public class Show_Loaded_Game {
         
         String output = "";
         
-        output += "[";
-        output += "{\"game_id\": \"no game spaces occupied\", \"player_chosen_game_piece\": \"no game spaces occupied\", \"player_chosen_game_space\": \"no game spaces occupied\", \"player_session\": \"no game spaces occupied\"}";
-        output += "]";
+        output += "{\"game_player_information\": [";
+        output += "{\"player_id\": \"no players\", \"player_full_name\": \"no players\", " +
+                "\"player_chosen_game_piece\": \"no players\"}], ";
+        output += "\"game_players\": [";
+        output += "{\"player_id\": \"no players\", \"player_full_name\": \"no players\", " +
+                "\"player_chosen_game_piece\": \"no players\"}], ";
+        output += "\"game_player_turns\": [";
+        output += "{\"player_id\": \"no players\", \"player_has_turn\": \"no players\"}], ";
+        output += "\"game_board\": [";
+        output += "{\"game_id\": \"no game spaces occupied\", " +
+                "\"player_chosen_game_piece\": \"no game spaces occupied\", " +
+                "\"player_chosen_game_space\": \"no game spaces occupied\", " +
+                "\"player_session\": \"no game spaces occupied\"}]}";
         
         return output;
     }
