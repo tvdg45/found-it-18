91e2893020707b8ce4cc2016d118f8aa0c92c6d0
Tim van der Graaff <timvdg45@gmail.com>
2022-02-26 19:11:23 -0600

    Update Admin_Delete_Chat_Messages.java

--- src/main/java/apps/Admin_Delete_Chat_Messages.java
+++ src/main/java/apps/Admin_Delete_Chat_Messages.java
@@ -7,6 +7,9 @@ import controllers.Control_Save_Chat_Messages;
 import java.io.IOException;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+
+import utilities.Find_and_replace;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
@@ -21,18 +24,44 @@ public class Admin_Delete_Chat_Messages {
     
     @RequestMapping(method = RequestMethod.POST)
     String home(
+			@RequestParam(value = "tic_tac_toe_game", defaultValue = "") String tic_tac_toe_game,
             @RequestParam(value = "delete_chat_messages", defaultValue = "") String delete_chat_messages
     ) {
 		
         Connection use_open_connection;
         
         try {
+			
             use_open_connection = Config.openConnection();
             
             Control_Save_Chat_Messages.use_connection = use_open_connection;
+			
+            String game_id_exception = "";
+            
+            ArrayList<String> find = new ArrayList<>();
+            ArrayList<String> replace = new ArrayList<>();
+            
+            find.add("(");
+            find.add(")");
+            find.add(", ");
+            
+            replace.add("");
+            replace.add("");
+            replace.add(",");
+			
+            try {
+                
+                String[] each_tic_tac_toe_game = Find_and_replace.find_and_replace(find, replace, tic_tac_toe_game).split(",");
+                
+                Control_Save_Chat_Messages.tic_tac_toe_game_id = each_tic_tac_toe_game;
+            } catch (NullPointerException e) {
+                
+                game_id_exception = "yes";
+            }
+			
             Control_Save_Chat_Messages.delete_chat_messages = delete_chat_messages;
             
-            if (delete_chat_messages.equals("Delete messages")) {
+            if (!(game_id_exception.equals("yes")) && delete_chat_messages.equals("Delete messages")) {
                 
                 return Control_Save_Chat_Messages.control_delete_all_chat_messages();
             } else {
